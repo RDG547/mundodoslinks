93e1464dc146eacf0c95c81b44611a80c46da1b3
RDG547 <rodrigotavaresvieira12@gmail.com>
2026-07-21 23:50:29 -0300

    fix: add steamstatic cdn domains to next.config.mjs remotePatterns

--- next.config.mjs
+++ next.config.mjs
@@ -10,6 +10,30 @@ const nextConfig = {
         protocol: 'https',
         hostname: '*.supabase.co',
       },
+      {
+        protocol: 'https',
+        hostname: 'cdn.akamai.steamstatic.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cdn.cloudflare.steamstatic.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'shared.akamai.steamstatic.com',
+      },
+      {
+        protocol: 'https',
+        hostname: '*.steamstatic.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'raw.githubusercontent.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'hydralinks.cloud',
+      },
     ],
   },
 };
